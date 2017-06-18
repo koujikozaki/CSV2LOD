fa4d44d885b478fde31d747dc8ae1cdf67d460fc
Kouji Kozaki <kozaki@ei.sanken.osaka-u.ac.jp>
2017-06-18 09:14:23 +0900

    語彙推薦の追加
    DCの推薦を少し追加

--- JavaScriptVer/js/data.js
+++ JavaScriptVer/js/data.js
@@ -7,7 +7,11 @@ var property_data =
 [
 	{  "prop" : "rdfs:label", "pname" : "ラベル", "datatype" : ["ITEM@ja", "ITEM@en"], "dtname" : ["文字列（日本語）", "文字列（英語）"], "keywords" : "名前 名称 名 label name", "output" : "ID rdfs:label ITEM",},
 	{  "prop" : "rdfs:comment", "pname" : "ラベル", "datatype" : ["ITEM@ja", "ITEM@en"], "dtname" : ["文字列（日本語）", "文字列（英語）"], "keywords" : "説明 コメント 概要 comment", "output" : "ID rdfs:label ITEM",},
-	{  "prop" : "rdfs:seeAlso", "pname" : "参考情報", "datatype" : "ITEM", "dtname" : "IRI", "keywords" : "参照 参考 参考情報 Web", "output" : "ID rdfs:seeAlso <ITEM>",},
+	{  "prop" : "dc:title", "pname" : "タイトル", "datatype" : ["ITEM@ja", "ITEM@en"], "dtname" : ["文字列（日本語）", "文字列（英語）"], "keywords" : "タイトル 書名", "output" : "ID dc:title ITEM",},
+	{  "prop" : "dc:creator", "pname" : "著者", "datatype" : ["ITEM@ja", "ITEM@en"], "dtname" : ["文字列（日本語）", "文字列（英語）"], "keywords" : "著者 作者", "output" : "ID dc:creator ITEM",},
+	{  "prop" : "dc:publisher", "pname" : "出版社", "datatype" : ["ITEM@ja", "ITEM@en"], "dtname" : ["文字列（日本語）", "文字列（英語）"], "keywords" : "出版社 発行者", "output" : "ID dc:publisher ITEM",},
+	{  "prop" : "dc:date", "pname" : "出版年", "datatype" : ["ITEM@ja", "ITEM@en"], "dtname" : ["文字列（日本語）", "文字列（英語）"], "keywords" : "年", "output" : "ID dc:date ITEM",},
+	{  "prop" : "rdfs:seeAlso", "pname" : "参考情報", "datatype" : "ITEM", "dtname" : "IRI", "keywords" : "参照 参考 参考情報 Web リンク URL", "output" : "ID rdfs:seeAlso <ITEM>",},
 	{  "prop" : "foaf:homepage", "pname" : "Webサイト", "datatype" : "ITEM", "dtname" : "IRI", "keywords" : "Web ページ サイト", "output" : "ID foaf:homepage <ITEM>",},
 	{  "prop" : "schma:name",  "pname" : "名前",  "datatype" : "ITEM@ja",  "dtname" : "文字列（日本語）",  "keywords" : "名前 名称 名", "output" : "ID schma:name ITEM",},
 	{  "prop" : "geo:lat",  "pname" : "緯度",  "datatype" : "ITEM^^xsd:double",  "dtname" : "小数（座標）",  "keywords" : "緯度 Y lat",  "output" : "ID geo:lat ITEM",  },
